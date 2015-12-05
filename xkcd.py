773a88689b857ed915f0b1dbef13b538dac8fb92
Sarthak Munshi <sarthakmunshi@gmail.com>
2015-12-05 12:37:35 +0530

    sqlite3 desktop.db support instead of appscript for osx

--- xkcd.py
+++ xkcd.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 from PIL import Image
+import sys
+import os
 
 def get_comic():
 	r = requests.get("http://xkcd.com/")
@@ -14,19 +16,42 @@ def get_comic():
 	return comic_link
 
 def download_comic():
-	f = open('00000001.png','wb')
+	f = open('00000000.png','wb')
 	f.write( requests.get( get_comic() ).content )
 	f.close()
 
 def create_wallpaper():
-	comic = Image.open('00000001.png','r')
+	download_comic()
+	comic = Image.open('00000000.png','r')
 	comic_w, comic_h = comic.size
+
 	background = Image.new('RGBA', (1440,900), (255,255,255,255))
 	background_w, background_h = background.size
+
 	offset = ( (background_w - comic_w) / 2, (background_h - comic_h) / 2 )
 	background.paste(comic, offset)
 	background.save('out.png')
 
+	os.remove("00000000.png")
 
-download_comic()
-create_wallpaper()
+def os_identification():
+	return sys.platform
+
+def set_wallpaper():
+	if os_identification() == "darwin":
+		create_wallpaper()
+		app_script = "sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db \"update data set value = '{}'\";"	.format("/Users/sarthakmunshi/Desktop/xkcd/out.png")
+		os.system(app_script)
+		os.system("killall Dock;")
+	elif os_identification() == "linux2":
+		create_wallpaper()
+		linux_path = "file:///"
+		mint = "gsettings set org.cinnamon.desktop.background picture-uri " + linux_path  
+		ubuntu = "gsettings set org.gnome.desktop.background picture-uri " + linux_path
+		try:
+			os.system(mint)
+		except:
+			os.system(ubuntu)
+
+if __name__ == '__main__':
+	set_wallpaper()
