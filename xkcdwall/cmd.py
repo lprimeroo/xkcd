4ae1c08802ebbbed9d8d3d0d0980680a3545f5b5
Adrian Moisey <adrian@changeover.za.net>
2016-10-02 20:56:24 +0200

    Whitespace

--- xkcdwall/cmd.py
+++ xkcdwall/cmd.py
@@ -6,30 +6,32 @@ from PIL import Image
 import sys
 import os
 import gi
-gi.require_version('Gdk', '3.0')
 from gi.repository import Gdk
+
+gi.require_version('Gdk', '3.0')
+
 
 def get_comic():
     r = requests.get("http://xkcd.com/")
 
     website_content = BeautifulSoup(r.content)
 
-    comic_div = website_content.findAll("div", { "id" : "comic" })
+    comic_div = website_content.findAll("div", {"id": "comic"})
 
     for comic in comic_div:
-        comic_link =  "http:" + comic.img['src']
+        comic_link = "http:" + comic.img['src']
     return comic_link
 
 
 def download_comic():
-    f = open('00000000.png','wb')
-    f.write( requests.get( get_comic() ).content )
+    f = open('00000000.png', 'wb')
+    f.write(requests.get(get_comic()).content)
     f.close()
 
 
 def create_wallpaper():
     download_comic()
-    comic = Image.open('00000000.png','r')
+    comic = Image.open('00000000.png', 'r')
     comic_w, comic_h = comic.size
     width = 1440
     height = 900
@@ -40,17 +42,19 @@ def create_wallpaper():
     except:
         print("No GDK Found ")
 
-    background = Image.new('RGBA', (width,height), (255,255,255,255))
+    background = Image.new('RGBA', (width, height), (255, 255, 255, 255))
     background_w, background_h = background.size
 
-    offset = ( (background_w - comic_w) / 2, (background_h - comic_h) / 2 )
+    offset = ((background_w - comic_w) / 2, (background_h - comic_h) / 2)
     background.paste(comic, offset)
     background.save('out.png')
 
     os.remove("00000000.png")
 
+
 def os_identification():
     return sys.platform
+
 
 def set_wallpaper():
     create_wallpaper()
